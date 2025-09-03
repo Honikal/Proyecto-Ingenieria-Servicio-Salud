786342badc0c41f5fccd47ea8e0f20b82c82d071
Honikal <81885736+Honikal@users.noreply.github.com>
2025-09-03 17:31:24 -0600

    Cambiamos unos errores que teniamos del landing page y creamos la pagina del login

---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderTab } from "../header-tab/header-tab";
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [HeaderTab],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css'
+})
+export class LandingPage {
+
+}
