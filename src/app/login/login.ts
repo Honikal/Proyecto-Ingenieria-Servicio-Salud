af5609f874f23744f6ec534bfd61f0dd60df8424
Honikal <81885736+Honikal@users.noreply.github.com>
2025-09-04 14:43:09 -0600

    Agregamos cambios visuales y de conexion posible al login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -12,7 +12,19 @@ export class Login {
   //Injectamos el router para permitir enrutamiento
   constructor(private router: Router) {};
 
+  onLoginClick(){
+    alert("Se hace el login ... por trabajar");
+  }
+
   onCancelClick(){
     this.router.navigate(['/']);
+  }
+
+  onSignUpClick(){
+    alert("Se redirige de login a registrar ... por trabajar");
+  }
+
+  onForgetPassword(){
+    alert("Se redirige a la página encargada de editar el password .. por trabajar")
   }
 }
