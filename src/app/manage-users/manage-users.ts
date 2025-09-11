1b85482cac8067d4cd6e217f4fb58e19ae992ea9
Honikal <81885736+Honikal@users.noreply.github.com>
2025-09-11 14:47:47 -0600

    Agregamos nuevos cambios del sistema para editar

--- src/app/manage-users/manage-users.ts
+++ src/app/manage-users/manage-users.ts
@@ -1,42 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
-
-interface User {
-  nombre: string;
-  correo?: string;
-  password: string;
-  telefono?: string;
-  area?: string;
-}
+import { NgIconComponent, provideIcons } from '@ng-icons/core'; 
+import { ionEye, ionEyeOff } from '@ng-icons/ionicons'; 
+import { User } from '../../models/user.model'; 
+import { FirebaseService } from '../services/firebase';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-manage-users',
-  imports: [],
+  imports: [NgIconComponent, ReactiveFormsModule],
   templateUrl: './manage-users.html',
-  styleUrl: './manage-users.css'
+  styleUrl: './manage-users.css',
+  providers: [provideIcons( { ionEye, ionEyeOff })]
 })
-export class ManageUsers {
-  //listaUsuarios : User[] = [];
-  listaUsuarios : User[] = [
-    { nombre: 'Juan Pérez', correo: 'juan@example.com', password: '1234', telefono: '8888-8888', area: 'Marketing' },
-    { nombre: 'Ana Gómez', correo: 'ana@example.com', password: '5678', telefono: '7777-7777', area: 'Finanzas' },
-    { nombre: 'Carlos López', correo: 'carlos@example.com', password: 'abcd', telefono: '6666-6666', area: 'TI' },
-  ];
+export class ManageUsers implements OnInit {
+  isEditing = false; //Función para definir si actualmente se está editando
+  showPassword = false;
+  user: User | null = null;
+  userForm!: FormGroup;
 
-  constructor(private router: Router) {};
+  //Creamos nuestro constructor y de acá extraemos al usuario
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private firebaseService: FirebaseService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  getInitials(usuario: User ) : string {
-    if (!usuario.nombre) return '??';
-    const parts = usuario.nombre.split(' ');
+  //La lógica de inicialización irá en el código de ngOnInit
+  async ngOnInit(){
+    //Creamos el form del usuario
+    this.userForm = this.fb.group({
+      fullname: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phone: [''],
+      area: ['']
+    })
+
+    const storedUser = localStorage.getItem('currentUser');
+    //Checamos la existencia de un usuario guardado
+    if (storedUser){
+      const parsed = JSON.parse(storedUser);
+      if (parsed.id){
+        //Obtenemos al usuario desde Firestore con el ID
+        this.user = await this.firebaseService.getUser(parsed.id);
+        console.log(`Usuario completo desde Firestore: ${this.user?.fullName}`);
+        this.cdr.detectChanges();
+      }
+    }
+  }
+
+  onPasswordToggle(){
+    this.showPassword = !this.showPassword;
+  }
+
+  getInitials() : string | null{
+    if (!this.user?.fullName) return null;
+    const parts = this.user.fullName.split(' ');
     return parts.slice(0, 2).map((p : string) => p[0].toUpperCase()).join('');
   }
 
-  onEditClick(index: number){
-    alert(`Editamos el usuario en la ubicación: ${index}`)
+  async onEditClick(){
+    //Checamos que el usuario exista
+    if (!this.user) return;
+
+    //Checamos que se encuentre en el lado de editar (o sea que va a guardar los datos)
+    if (this.isEditing){
+      if (this.userForm.valid){
+        const storedUser = localStorage.getItem('currentUser');
+        if (storedUser) {
+        //Guardamos los cambios editados
+          const parsed = JSON.parse(storedUser);
+          await this.firebaseService.updateUser(parsed.id, this.user);
+          alert("Cambios guardados correctamente");
+          this.isEditing = false; //Quitamos el modo de edición
+        }
+      } else {
+        alert("Por favor completa los cargos requeridos");
+      }
+    } else {
+      //Pasamos al modo de edición
+      this.isEditing = true;
+    }
   }
 
-  onDeleteClick(index: number){
-    alert(`Eliminamos el usuario en la ubicación: ${index}`)
+  onDeleteClick(){
+    alert(`Eliminamos el usuario en la ubicación: `);
   }
 
   onGoLandingPage(){
--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { collection, Firestore, addDoc, query, where, getDocs, updateDoc } from '@angular/fire/firestore';
+import { collection, Firestore, addDoc, doc, query, where, getDoc, getDocs, updateDoc } from '@angular/fire/firestore';
 import { User } from '../../models/user.model';
 import * as bcrypt from 'bcryptjs';
 //import { v4 as uuidv4 } from 'uuid';
@@ -48,23 +48,43 @@ export class FirebaseService {
     //await this.sendVerificationEmail(newUser.email, verificationToken);
   }
 
-async login(email: string, password: string){
-  const usersRef = collection(this.firestore, 'users');
-  const q = query(usersRef, where('email', '==', email));
-  const snapshot = await getDocs(q);
+  async login(email: string, password: string){
+    const usersRef = collection(this.firestore, 'users');
+    const q = query(usersRef, where('email', '==', email));
+    const snapshot = await getDocs(q);
 
-  if (snapshot.empty) {
-    return null;
+    if (snapshot.empty) {
+      return null;
+    }
+
+    const userDoc = snapshot.docs[0];
+    const user = userDoc.data() as User;
+    const id = userDoc.id;
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) return null;
+
+    return { ...user, id };;
   }
 
-  const userDoc = snapshot.docs[0];
-  const user = userDoc.data() as User;
-  const id = userDoc.id;
+  async getUser(userID: string){
+    //Agarramos el path directo para extraer los datos del usuario
+    const userRef = doc(this.firestore, 'users', userID);
+    const snapshot = await getDoc(userRef);
 
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) return null;
+    //Checamos el caso que el usuario o el path directo al usuario no exista
+    if (!snapshot.exists()){
+      return null;
+    }
 
-  return { ...user, id };;
-}
+    //Retornamos el posible usuario
+    return { id: snapshot.id, ...(snapshot.data() as User) };
+  }
+
+  async updateUser(userID: string, updatedData: Partial<User>){
+    //Agarramos el path directo para extraer los datos del usuario
+    const userRef = doc(this.firestore, 'users', userID);
+    await updateDoc(userRef, updatedData);
+  }
 
 }
