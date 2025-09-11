48a06c7909b0159df43384c821382ec5523750b1
Emmanuel MJ <emmajose1703@gmail.com>
2025-09-10 20:22:04 -0600

    display datos
    ahora que hay datos en la base se probó y se arregló un problema de async que no permitía mostrar los datos

--- src/app/manage-users/manage-users.ts
+++ src/app/manage-users/manage-users.ts
@@ -1,33 +1,31 @@
-// manage-users.ts
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase';
 import { User } from '../../models/user.model';
+import { Observable } from 'rxjs';
+import { NgFor, AsyncPipe } from '@angular/common'; // 👈 importa AsyncPipe
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.html',
   styleUrl: './manage-users.css',
-  standalone: true
+  standalone: true,
+  imports: [NgFor, AsyncPipe] 
 })
-export class ManageUsers implements OnInit {
-  listaUsuarios: User[] = [];
+export class ManageUsers {
+  userList$: Observable<User[]>;
 
   constructor(
     private router: Router,
     private firebaseService: FirebaseService
-  ) {}
-
-  ngOnInit(): void {
-    this.firebaseService.getUsers().subscribe((users) => {
-      this.listaUsuarios = users;
-    });
+  ) {
+    this.userList$ = this.firebaseService.getUsers();
   }
 
   getInitials(usuario: User): string {
     if (!usuario.fullName) return '??';
     const parts = usuario.fullName.split(' ');
-    return parts.slice(0, 2).map((p: string) => p[0].toUpperCase()).join('');
+    return parts.slice(0, 2).map(p => p[0].toUpperCase()).join('');
   }
 
   onEditClick(index: number) {
