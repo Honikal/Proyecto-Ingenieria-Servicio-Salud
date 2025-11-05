7661c425ba5cad6ab68a333b8a91a201389825ed
Honikal <81885736+Honikal@users.noreply.github.com>
2025-11-04 18:34:10 -0600

    Antes de subir cambios al register o a otros, creamos el sistema de otp y otp service

--- email_server/server.ts
+++ email_server/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { sendEmail } from "./sendEmail";
+import { requestOTP, verifyOTP, resendOTP } from "./otp"
 
 const app = express();
 app.use(cors());
@@ -19,6 +20,57 @@ app.post("/send-email", async (req, res) => {
     } catch(error){
         console.error("❌ Error en /send-email:", error);
         res.status(500).json({ success: false, msg: "Error sending email" });
+    }
+});
+
+app.post("/auth/request-otp", async (req, res) => {
+    //Encargado de solicitar el otp
+    console.log("📩 Recibí request en /auth/request-otp:", req.body);
+
+    //Extraemos el destino del correo, el asunto y la descripción  
+    const {email, purpose} = req.body;
+
+    try {
+        const pendingId = await requestOTP(purpose, email)
+        console.log("✅ Correo enviado, se ha adjuntado el OTP con pendingID:", pendingId);
+        res.json({ success: true, pendingId });
+    } catch(error: any){
+        console.error("❌ Error en /auth/request-otp:", error);
+        res.status(500).json({ success: false, message: error.message});
+    }
+});
+
+app.post("/auth/verify-otp", async (req, res) => {
+    //Encargado de solicitar el otp
+    console.log("📩 Recibí request en /auth/verify-otp:", req.body);
+
+    //Extraemos el destino del correo, el asunto y la descripción  
+    const { pendingId, code, purpose } = req.body;
+
+    try {
+        const result = await verifyOTP( pendingId, code, purpose)
+        console.log("✅ Se ha ingresado el código OTP de forma correcta, se ha enviado el mensaje:", result);
+        res.json({ success: true, msg: result });
+    } catch(error: any){
+        console.error("❌ Error en /auth/verify-otp:", error);
+        res.status(500).json({ success: false, message: error.message});
+    }
+});
+
+app.post("/auth/resend-otp", async (req, res) => {
+    //Encargado de solicitar el otp
+    console.log("📩 Recibí request en /auth/resend-otp:", req.body);
+
+    //Extraemos el destino del correo, el asunto y la descripción  
+    const { pendingId, purpose, email } = req.body;
+
+    try {
+        await resendOTP(pendingId, purpose, email);
+        console.log("✅ Correo enviado, se ha vuelto a enviar un nuevo código");
+        res.json({ success: true });
+    } catch(error: any){
+        console.error("❌ Error en /auth/resend-otp:", error);
+        res.status(500).json({ success: false, message: error.message});
     }
 });
 
---
+++ src/app/services/otp.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OTPService {
+    //Esta clase se encargará de conectar con el backend y se encargará de tanto enviar correos de notificacion
+    //como los correos esperando un código OTP
+
+    private API_URL = 'http://localhost:4000'; //TODO Cambiar la url privada por una funcional a la hora de hostear
+
+    constructor(private http: HttpClient) {}
+
+    requestOTP(email: string, payload: any){
+        return this.http.post(`${this.API_URL}/auth/request-otp`, 
+        {
+            email,
+            purpose: "register",
+        });
+    }
+
+    verifyOTP(pendingId: string, code: string){
+        return this.http.post(`${this.API_URL}/auth/verify-otp`, 
+        {
+            pendingId,
+            code, 
+            purpose: "register"
+        });
+    }
+
+    resendOTP(pendingId: string, email: string){
+        return this.http.post(`${this.API_URL}/auth/resend-otp`, 
+        {
+            pendingId,
+            purpose: "register",
+            email,
+        });
+    }
+}
+
