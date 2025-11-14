39345f21dd9bc7df9830b0f5f0fc01dc4dfe85bc
Honikal <81885736+Honikal@users.noreply.github.com>
2025-11-13 19:20:04 -0600

    Cambiamos sistema para enviar correos, y ahora probamos que hosteado funcione

--- email_server/sendEmail.ts
+++ email_server/sendEmail.ts
@@ -1,45 +1,28 @@
-import nodemailer from 'nodemailer';
-import { google } from "googleapis";
+import { Resend } from "resend";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const CLIENT_ID = process.env.GMAIL_CLIENT_ID!;
-const CLIENT_SECRET = process.env.GMAIL_CLIENT_SECRET!;
-const REFRESH_TOKEN = process.env.GMAIL_REFRESH_TOKEN!;
-const USER_MAIL = process.env.GMAIL_USER!;
+const API_KEY = process.env.RESEND_API_KEY;
+const ORIGIN_EMAIL = process.env.GMAIL_USER;
+const resend = new Resend(API_KEY);
 
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
-
-export const sendEmail = async (toEmail: string, subject: string, htmlContent: string) => {
+export async function sendEmail (
+    toEmail: string,
+    subject: string,
+    htmlContent: string
+) {
     /*Función encargada de enviar un mensaje cualquiera al correo del usuario, para notificar de
     información importante a considerar*/
     try {
-        const accessToken = await oAuth2Client.getAccessToken();
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                type: "OAUTH2",
-                user: USER_MAIL,
-                clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: accessToken?.token || "",
-            },
-        });
-
-        const mailOption = {
-            from: `Servicio de Salud Ocupacional Brindado por el tec <${USER_MAIL}>`,
+        const response = await resend.emails.send({
+            from: ORIGIN_EMAIL || "Servicio Salud <onboarding@resend.dev>",
             to: toEmail,
             subject: subject,
-            html: htmlContent,
-            text: `Tu código de verificación es: ${extractCodeFromHTML(htmlContent)}. Por favor ingrésalo en la aplicación.`
-        };
-
-        const result = await await transporter.sendMail(mailOption);
-        return result;
+            html: htmlContent
+        })
+        console.log("📨 Email enviado:", response);
+        return response;
     } catch (error){
         console.error("Error enviando el correo desde Backend: ", error);
         throw error;
